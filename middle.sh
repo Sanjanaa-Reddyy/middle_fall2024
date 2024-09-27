ae086324f181352cfa605934428c3333811c3870
Sanjana <sanjanareddy1616@gmail.com>
2024-09-27 20:34:43 +0000

    set mistake

--- middle.sh
+++ middle.sh
@@ -1,1 +1,7 @@
-head -n 9 ../alkanes/cubane.pdb | tail -n 3
+#script to return the middle text of a file
+# created on Sep 27, 2024
+# creator Sanjana
+# Usage: 
+# include in first position, file path and name via command line 
+
+head -n "$2"  "$1" | tail -n "$3"
