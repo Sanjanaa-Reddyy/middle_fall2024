24a4bc318f15e5ca92ec9222d6022248a014ba01
Sanjana <sanjanareddy1616@gmail.com>
2024-09-27 20:09:45 +0000

    added how to use this script

--- middle.sh
+++ middle.sh
@@ -1,5 +1,7 @@
 # script to return the middle text of a file
 # created on Sep 27, 2024
-# creator Sanjana 
+# creator Sanjana
+# Usage: 
+# include in first position, file path and name via command line 
 
 head -n 9 "$1" | tail -n 3
